f97d57184d9059c37998785d4ef291b597e52b7e
e6ntff <pashatar2@gmail.com>
2024-01-03 13:26:28 +0300

    added project

--- src/settings/data.js
+++ src/settings/data.js
@@ -4,6 +4,9 @@ import pic1_3 from '../media/expense-manager-3.gif';
 
 import pic2_1 from '../media/tic-tac-toe-1.gif';
 
+import pic3_1 from '../media/typing-test-1.gif';
+import pic3_2 from '../media/typing-test-2.gif';
+
 export const description = {
 	skills: {
 		title: 'skills',
@@ -14,8 +17,9 @@ export const description = {
 export const projects = [
 	{
 		title: 'expense manager',
-		description: 'App where you can store your expenses, divide it by categories and manage',
-		grid: [2,2],
+		description:
+			'App where you can store your expenses, divide it by categories and manage',
+		grid: [2, 2],
 		pictures: [
 			{ pic: pic1_1, area: '1/1/2/2' },
 			{ pic: pic1_2, area: '1/2/2/3' },
@@ -24,8 +28,9 @@ export const projects = [
 	},
 	{
 		title: 'tic tac toe',
-		description: 'You can play with other player on the same device or with computer',
-		grid: [1,1],
+		description:
+			'You can play with other player on the same device or with computer',
+		grid: [1, 1],
 		pictures: [
 			{
 				pic: pic2_1,
@@ -33,5 +38,19 @@ export const projects = [
 			},
 		],
 	},
-	
+	{
+		title: 'typing test',
+		description: 'How fast you can type? You can check it there',
+		grid: [1, 2],
+		pictures: [
+			{
+				pic: pic3_1,
+				area: '1/1/2/2',
+			},
+			{
+				pic: pic3_2,
+				area: '2/1/3/2',
+			},
+		],
+	},
 ];
